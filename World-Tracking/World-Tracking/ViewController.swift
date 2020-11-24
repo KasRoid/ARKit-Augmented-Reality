2dbaf76a74354b8ddc7375eac72bd303f08aecdd
KasRoid <KasRoid@gmail.com>
2020-11-24 23:26:05 +0900

    feat: Position nodes

--- World-Tracking/World-Tracking/ViewController.swift
+++ World-Tracking/World-Tracking/ViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Properties
     private let sceneView = ARSCNView()
     private let configuration = ARWorldTrackingConfiguration()
+    private let button = UIButton(type: .system)
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSceneView()
         setupUI()
+    }
+    
+    // MARK: - Selectors
+    @objc
+    private func handleButton(_ sender: UIButton) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        node.position = SCNVector3(0.2, 0.2, -0.2)
+        sceneView.scene.rootNode.addChildNode(node)
     }
 }
 
@@ -34,6 +47,15 @@ extension ViewController {
             sceneView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             sceneView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             sceneView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        button.setTitle("Button", for: .normal)
+        button.addTarget(self, action: #selector(handleButton(_:)), for: .touchUpInside)
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
         ])
     }
 }
