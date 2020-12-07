b4fe108674d9ec347283f166fc1922c683fd0817
KasRoid <KasRoid@gmail.com>
2020-12-07 21:54:06 +0900

    feat: Horizontal Lava

--- Floor-is-Lava/Floor-is-Lava/ViewController.swift
+++ Floor-is-Lava/Floor-is-Lava/ViewController.swift
@@ -26,20 +26,42 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: - AR Related
+extension ViewController {
+    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "lava")
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
+        lavaNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        return lavaNode
+    }
+}
+
 // MARK: - ARSCNViewDelegate
 extension ViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAhnchor = anchor as? ARPlaneAnchor else { return }
+        let lavaNode = createLava(planeAnchor: planeAhnchor)
+        node.addChildNode(lavaNode)
         print("new flat surface dtectd, new ARPlaneAnchor added")
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let planeAhnchor = anchor as? ARPlaneAnchor else { return }
+        node.enumerateChildNodes { (childeNode, _) in
+            childeNode.removeFromParentNode()
+        }
+        let lavaNode = createLava(planeAnchor: planeAhnchor)
+        node.addChildNode(lavaNode)
         print("Updating floor's anchor")
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        print("")
+        guard let planeAhnchor = anchor as? ARPlaneAnchor else { return }
+        node.enumerateChildNodes { (childeNode, _) in
+            childeNode.removeFromParentNode()
+        }
     }
 }
 
@@ -61,4 +83,8 @@ extension ViewController {
             sceneView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
+}
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi / 180 }
 }
