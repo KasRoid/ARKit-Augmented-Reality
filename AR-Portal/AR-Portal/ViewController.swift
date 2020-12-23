efd894ea2e761caea1e71cf2fa5094d4cdc58fc8
KasRoid <KasRoid@gmail.com>
2020-12-23 10:44:04 +0900

    feat: Building the Portal

--- AR-Portal/AR-Portal/ViewController.swift
+++ AR-Portal/AR-Portal/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     final private let sceneView = ARSCNView()
+    final private let detectionLabel = UILabel()
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -22,18 +23,49 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: - Helpers
+extension ViewController {
+    func showDetectionLabel() {
+        DispatchQueue.main.async { self.detectionLabel.isHidden = false }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3,
+                                      execute: { self.detectionLabel.isHidden = true })
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        showDetectionLabel()
+    }
+}
+
 // MARK: - UI
 extension ViewController {
     final private func setAR() {
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = .horizontal
+        sceneView.delegate = self
         sceneView.session.run(configuration)
     }
     
+    final private func setBasics() {
+        detectionLabel.text = "Plane Detected"
+        detectionLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        detectionLabel.textColor = .black
+        detectionLabel.isHidden = true
+    }
+    
     final private func setUI() {
-        view.addSubview(sceneView)
+        setBasics()
+        [sceneView, detectionLabel].forEach {
+            view.addSubview($0)
+        }
         sceneView.snp.makeConstraints {
             $0.edges.equalToSuperview()
+        }
+        detectionLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.centerX.equalToSuperview()
         }
     }
 }
