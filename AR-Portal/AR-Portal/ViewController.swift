b7cf8f782dfe3971394db3e6e19dffeba29f096d
KasRoid <KasRoid@gmail.com>
2020-12-23 22:57:10 +0900

    feat: Cube Map

--- AR-Portal/AR-Portal/ViewController.swift
+++ AR-Portal/AR-Portal/ViewController.swift
@@ -30,12 +30,58 @@ extension ViewController {
         DispatchQueue.main.asyncAfter(deadline: .now() + 3,
                                       execute: { self.detectionLabel.isHidden = true })
     }
+    
+    func addPortal(raycastResult: ARRaycastResult) {
+        let portalScene = SCNScene(named: "portal.scnassets/Portal.scn")!
+        let portalNode = portalScene.rootNode.childNode(withName: "Portal", recursively: false)!
+        let transform = raycastResult.worldTransform
+        let planeXposition = transform.columns.3.x
+        let planeYposition = transform.columns.3.y
+        let planeZposition = transform.columns.3.z
+        portalNode.position = SCNVector3(planeXposition, planeYposition, planeZposition)
+        addPlane(nodeName: "roof", portalNode: portalNode, imageName: "top")
+        addPlane(nodeName: "floor", portalNode: portalNode, imageName: "bottom")
+        addWalls(nodeName: "backWall", portalNode: portalNode, imageName: "back")
+        addWalls(nodeName: "sideWallA", portalNode: portalNode, imageName: "sideA")
+        addWalls(nodeName: "sideWallB", portalNode: portalNode, imageName: "sideB")
+        addWalls(nodeName: "sideDoorA", portalNode: portalNode, imageName: "sideDoorA")
+        addWalls(nodeName: "sideDoorB", portalNode: portalNode, imageName: "sideDoorB")
+        sceneView.scene.rootNode.addChildNode(portalNode)
+    }
+    
+    func addWalls(nodeName: String, portalNode: SCNNode, imageName: String) {
+        let child = portalNode.childNode(withName: nodeName, recursively: true)
+        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: imageName)
+        child?.geometry?.firstMaterial?.isDoubleSided = true
+    }
+    
+    func addPlane(nodeName: String, portalNode: SCNNode, imageName: String) {
+        let child = portalNode.childNode(withName: nodeName, recursively: true)
+        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: imageName)
+        child?.geometry?.firstMaterial?.isDoubleSided = true
+    }
 }
 
+// MARK: - ARSCNViewDelegate
 extension ViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        guard let _ = anchor as? ARPlaneAnchor else { return }
         showDetectionLabel()
+    }
+}
+
+// MARK: - Selectors
+extension ViewController {
+    @objc
+    private func handleTap(_ sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else { return }
+        let touchLocation = sender.location(in: sceneView)
+        let raycastQuery = sceneView.raycastQuery(from: touchLocation, allowing: .existingPlaneGeometry, alignment: .horizontal)
+        guard let raycast = raycastQuery else { return }
+        let raycastResults = sceneView.session.raycast(raycast)
+        if !raycastResults.isEmpty {
+            addPortal(raycastResult: raycastResults.first!)
+        }
     }
 }
 
@@ -53,6 +99,9 @@ extension ViewController {
         detectionLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
         detectionLabel.textColor = .black
         detectionLabel.isHidden = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        sceneView.addGestureRecognizer(tapGesture)
     }
     
     final private func setUI() {
